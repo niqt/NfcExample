58227bd063d03d186d8de33143bfbc773e248c7e
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-04-21 16:00:52 +0200

    Initial Commit

---
+++ NfcExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NfcExample
+//
+//  Created by Nicola De Filippo on 21/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NfcExample/NfcExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NfcExampleApp.swift
+//  NfcExample
+//
+//  Created by Nicola De Filippo on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NfcExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
